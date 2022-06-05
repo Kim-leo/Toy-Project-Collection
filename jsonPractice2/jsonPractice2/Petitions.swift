ce4c681b80d5833433f6a1ab057941e13ba3abb8
Kim - Leo <77371366+Kim-leo@users.noreply.github.com>
2022-06-05 18:58:27 +0900

    Petitions 주석 추가

--- jsonPractice2/jsonPractice2/Petitions.swift
+++ jsonPractice2/jsonPractice2/Petitions.swift
@@ -10,3 +10,12 @@ import Foundation
 struct Petitions: Codable {
     var results: [Petition]
 }
+
+/*
+ 각각의 Petition을 담는 Petitions라는 Petition으로 구성된 배열을 results라는 이름으로 정의한다.
+ 
+ ViewController에서 class내부에
+ " var petitions = [Petition]() "
+ 가 정의됐다면, JSON을 parse할 수 있다.
+ 즉, JSON 파일 내에서 원하는 내용을 처리하여 코드로 다룰 수 있게 할 수 있다.
+ */
